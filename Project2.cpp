e3dd934fcad36343685f82daae78573b7b1bd3b7
2000AP <88373411+2000AP@users.noreply.github.com>
2021-09-01 17:04:23 +0530

    finding size of datatype and product of two no.
    In this Project 1 consist of finding size of datatype such as char, int, float, double .
    Project 2 consist of finding product of 2 number entered by user.

---
+++ Project2.cpp
@@ -0,0 +1,13 @@
+////Program to find product of two numbers entered by users
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float num1,num2, product;
+    cout<<"Enter two Numbers: ";
+    cin>>num1;
+    cin>>num2;
+    product = num1*num2;
+    cout<<"Product = "<<product<<endl;
+}
